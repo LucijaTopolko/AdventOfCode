5098a48206cbdc5456312478d41f35137479806a
Lucija Topolko <53304190+LucijaTopolko@users.noreply.github.com>
2024-12-02 06:40:43 +0100

    day 2

---
+++ 2024/day2.py
@@ -0,0 +1,46 @@
+file = open("2024/inputi/input.txt", "r")
+lines = file.readlines()
+for i in range(0, len(lines)):
+    lines[i] = lines[i].split("\n")[0]
+
+def issafe(l):
+    direction = l[0]< l[1]
+    for j in range(1, len(l)):
+        if direction and l[j - 1] >= l[j]:
+            return False
+        else:
+            if not direction and l[j-1] <= l[j]:
+                return False
+        if not 1 <= abs(l[j-1] - l[j]) <= 3:
+            return False
+    return True
+    
+
+print("***** PART 1 *****")
+
+count = 0
+
+for line in lines:
+    l = list(map(int, line.split()))
+    if issafe(l):
+        count += 1
+
+print(count)
+
+print("***** PART 2 *****")
+
+count = 0
+
+for line in lines:
+    l = list(map(int, line.split()))
+    if issafe(l):
+        count += 1
+        continue
+
+    for i in range(len(l)):
+        temp = l[:i] + l[i+1:]
+        if issafe(temp):
+            count += 1
+            break
+    
+print(count)
